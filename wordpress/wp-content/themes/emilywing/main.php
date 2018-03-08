d1f7837190080365200d8bbae0ad65e0d1f78ba6
emwing <emwing@emily-wing.com>
2018-03-08 17:13:39 -0500

    Check-In 8.2 Final

--- wordpress/wp-content/themes/emilywing/main.php
+++ wordpress/wp-content/themes/emilywing/main.php
@@ -2,8 +2,13 @@
 <main>
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> <!-- if there are posts, while there are still posts to show, run the_post function -->
+
     <!-- now tell wordpress to do something if posts exist -->
-    <p>Here's a post</p>
+    <div class="postBody">
+      <!-- < php echo $post->post_title; > manuallly grab wordpress variable $post, which is an object, then use -> to access key info -->
+      <?php the_title('<h2>','</h2>'); ?>
+      <?php the_content(); ?>
+    </div>
 
   <?php endwhile; else : ?>
     <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
